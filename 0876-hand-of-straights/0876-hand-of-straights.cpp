acedd8fe650b6991bec9f408e9aee1d773be7764
Dheeraj Yadav <145187556+dheerajyadav0@users.noreply.github.com>
2025-07-11 04:53:08 +0530

    Time: 32 ms (75.69%), Space: 32.5 MB (20.87%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+
+        if (n % groupSize != 0) return false;
+
+       unordered_map<int, int> mpp;
+        for (int i = 0; i < n; i++) {
+            mpp[hand[i]]++;
+        }
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto& it : mpp) {
+            pq.push(it.first);
+        }
+
+        while (!pq.empty()) {
+            int first = pq.top();
+
+            for (int i = 0; i < groupSize; i++) {
+                int curr = first + i;
+
+                if (mpp.find(curr) == mpp.end()) return false;
+
+                mpp[curr]--;
+
+                if (mpp[curr] == 0) {
+                    mpp.erase(curr);
+
+                    if (!pq.empty() && pq.top() == curr) {
+                        pq.pop();
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
